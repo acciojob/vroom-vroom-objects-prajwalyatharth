8a8a794f89f6064d989055d5f42ce276355a0200
Prajwal Kumar <94397677+prajwalyatharth@users.noreply.github.com>
2024-12-23 21:39:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	    return `${this.make} ${this.model}`;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	    // Call the Car constructor with the current context and arguments
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
