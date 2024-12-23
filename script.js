b43c8d753948a1550a9117d9cf0728cd834ba0b3
Prajwal Kumar <94397677+prajwalyatharth@users.noreply.github.com>
2024-12-23 21:40:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-// Complete the js code
+// Car constructor function
 function Car(make, model) {
-	    return `${this.make} ${this.model}`;
+    this.make = make;
+    this.model = model;
 }
 
+// Add getMakeModel method to Car prototype
+Car.prototype.getMakeModel = function () {
+    return `${this.make} ${this.model}`;
+};
+
+// SportsCar constructor function
 function SportsCar(make, model, topSpeed) {
-	    // Call the Car constructor with the current context and arguments
+    // Call the Car constructor with the current context and arguments
     Car.call(this, make, model);
     this.topSpeed = topSpeed;
 }
+
+// Inherit the Car prototype in SportsCar prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Reset the constructor reference to SportsCar
+SportsCar.prototype.constructor = SportsCar;
+
+// Add getTopSpeed method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+    return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
